0dd5f37efdd8bd02fc927e479e9b598fc1fe05bc
DumezErika <47596821+DumezErika@users.noreply.github.com>
2020-12-08 17:47:40 +0100

    Add files via upload

--- exercices livre/Chapitre 5/exercice6b.py
+++ exercices livre/Chapitre 5/exercice6b.py
@@ -18,8 +18,26 @@ default = default_.copy()
 def boot_fn(dataFrame, index):
     df = dataFrame.iloc[index, :]
     X = df[['income', 'balance']]
-    y = df[['default']]
+    y = df['default']
     clf = LogisticRegression().fit(X,y)
     return(clf.coef_, clf.intercept_)
+    
+def boot_se(dataFrame, index):
+    df = dataFrame.iloc[index,:]
+    X = df[['income', 'balance']]
+    y = df['default']
+    clf = LogisticRegression().fit(X,y)
+    predProbs = clf.predict_proba(X)
+    X_design = np.hstack([np.ones((X.shape[0],1)), X])
+    V = np.diagflat(np.product(predProbs, axis=1))
+    covLogit = np.linalg.inv(np.dot(np.dot(X_design.T,V), X_design))
+    return(np.sqrt(np.diag(covLogit)))
 
-print(boot_fn(default, [i for i in range(10000)]))
+def boot(dataFrame, n):
+    standard_errors = []
+    for i in range(n):
+        index = np.random.choice([i for i in range(len(dataFrame))], len(dataFrame))
+        standard_errors.append(boot_se(dataFrame, index))
+    return pd.DataFrame(standard_errors).mean()
+
+print(boot(default,100))
