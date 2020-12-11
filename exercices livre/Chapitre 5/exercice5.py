c08faa1157d57ea86a5f95dd66dd366b9a111b34
DellaBonaSarah <47570437+DellaBonaSarah@users.noreply.github.com>
2020-12-11 09:20:04 +0100

    Update exercice5.py

--- exercices livre/Chapitre 5/exercice5.py
+++ exercices livre/Chapitre 5/exercice5.py
@@ -1,26 +1,14 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn import linear_model
-from random import sample
 import numpy as np
-import statsmodels.api as sm
-import statsmodels.formula.api as smf
-import sklearn.linear_model as lm
-from statsmodels.sandbox.regression.predstd import wls_prediction_std
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
-sns.set_theme(style='darkgrid')
 
 default = pd.read_csv("Default.csv")
 
 #a)
 X = default[['income', 'balance']]
-y = default[['default']]
+y = default['default']
 clf = LogisticRegression().fit(X,y)
 
 #b)
@@ -33,7 +21,7 @@ default_copy = default.copy()
 default_copy.loc[default_copy['student'] == 'No', 'student'] = 0
 default_copy.loc[default_copy['student'] == 'Yes', 'student'] = 1
 A = default_copy[['income', 'balance', 'student']]
-b = default_copy[['default']]
+b = default_copy['default']
 A_train, A_test, b_train, b_test = train_test_split(A, b, test_size = 0.33, random_state=4)
 clf2 = LogisticRegression().fit(A_train,b_train)
 print(confusion_matrix(b_test, clf2.predict(A_test)))
