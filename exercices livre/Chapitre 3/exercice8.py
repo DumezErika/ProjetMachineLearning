6e671c1f3662549a5f21e1a24998c0ac21877f52
DellaBonaSarah <47570437+DellaBonaSarah@users.noreply.github.com>
2020-12-11 08:19:38 +0100

    Add files via upload

--- exercices livre/Chapitre 3/exercice8.py
+++ exercices livre/Chapitre 3/exercice8.py
@@ -1,44 +1,42 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn import linear_model
-import numpy as np
-import statsmodels.api as sm
-import statsmodels.formula.api as smf
 import sklearn.linear_model as lm
+import numpy as np
+import statsmodels.formula.api as smf
 from statsmodels.sandbox.regression.predstd import wls_prediction_std
-sns.set_theme(style='darkgrid')
+sns.set_theme(style="darkgrid")
+
 
 auto = pd.read_csv("Auto.csv")
-tab = auto.copy()
+df = auto.copy()
+df = df.loc[b["horsepower"]!= "?"]
+temp = pd.to_numeric(df["horsepower"])
+df["horsepower"] = temp
 
-horsepower = [float(i) for i in tab.loc[tab['horsepower'] != '?', 'horsepower'].tolist()]
-a = np.reshape(horsepower, (-1,1))
-b = tab.loc[tab['horsepower'] != '?', 'mpg'].tolist()
-A = sm.add_constant(a)
-lm_fit = sm.OLS(b,A).fit()
-print(lm_fit.summary())
-
+#a
+#Première approche
+reg = lm.LinearRegression()
+X = np.reshape(df.horsepower.tolist(),(-1,1))
+y = df.mpg.tolist()
+reg.fit(X,y)
+print(reg.coef_)
+print(reg.intercept_)
 print(reg.predict([[98]]))
 
-"""
-prstd, iv_l, iv_u = wls_prediction_std(lm_fit)
-fig, ax = plt.subplots(figsize=(8,6))
+#Deuxième approche
+lm_fit = smf.ols(data = df, formula = 'mpg~horsepower').fit()
+print(lm_fit.summary())
+_, iv_l, iv_u = wls_prediction_std(lm_fit)
 
-ax.plot(a,b,'o', label = "data")
-ax.plot(a, lm_fit.fittedvalues, 'r--.', label="OLS")
-ax.plot(a, iv_u, 'r--')
-ax.plot(a, iv_l, 'r--')
-ax.legend(loc='best')
+#b
+plt.scatter(df.horsepower, df.mpg)
+plt.plot(df.horsepower, iv_u, 'r--')
+plt.plot(df.horsepower, iv_l, 'r--')
+X = pd.DataFrame({'horsepower': [df.horsepower.min(), df.horsepower.max()]})
+y_pred = lm_fit.predict(X)
+plt.plot(X, y_pred, c='red')
 
-sns.residplot(a, b)
-
+#c
+g = sns.residplot(df.horsepower, df.mpg)
 plt.show()
-"""
-
-"""
-plt.scatter(a, b, color='black')
-plt.plot(a, reg.predict(a), color='blue', linewidth=2)
-
-plt.show()
-"""
