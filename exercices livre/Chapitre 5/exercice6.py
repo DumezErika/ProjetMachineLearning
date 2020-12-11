72aee360554f7dce61f27ab5a2d6398c917eb6b3
DellaBonaSarah <47570437+DellaBonaSarah@users.noreply.github.com>
2020-12-11 09:37:29 +0100

    Update exercice6.py

--- exercices livre/Chapitre 5/exercice6.py
+++ exercices livre/Chapitre 5/exercice6.py
@@ -1,24 +1,17 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn import linear_model
-from random import sample
 import numpy as np
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
-import sklearn.linear_model as lm
-from statsmodels.sandbox.regression.predstd import wls_prediction_std
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix
-sns.set_theme(style='darkgrid')
 
 default_ = pd.read_csv("Default.csv")
 default = default_.copy()
 
+# a
+# Première approche
 X = default[['income', 'balance']]
-y = default[['default']]
+y = default['default']
 clf = LogisticRegression().fit(X,y)
-
 
 predProbs = clf.predict_proba(X)
 X_design = np.hstack([np.ones((X.shape[0],1)), X])
@@ -29,10 +22,40 @@ standard_errors = np.sqrt(np.diag(covLogit))
 print("Coefficients : " , clf.coef_ , clf.intercept_)
 print("Standard errors : ", standard_errors)
 
+#Deuxième approche
 default.loc[default['default'] == 'No', 'default'] = int(1)
 default.loc[default['default'] == 'Yes', 'default'] = int(0)
 
 f = 'default ~ income + balance'
-#model = smf.logit(str(f), default).fit()
 model = smf.glm(formula = f, data = default, family = sm.families.Binomial()).fit()
 print(model.summary())
+
+# b
+def boot_fn(dataFrame, index):
+    df = dataFrame.iloc[index, :]
+    X = df[['income', 'balance']]
+    y = df['default']
+    clf = LogisticRegression().fit(X,y)
+    return(clf.coef_, clf.intercept_)
+
+# c
+def boot_se(dataFrame, index):
+    df = dataFrame.iloc[index,:]
+    X = df[['income', 'balance']]
+    y = df['default']
+    clf = LogisticRegression().fit(X,y)
+    predProbs = clf.predict_proba(X)
+    X_design = np.hstack([np.ones((X.shape[0],1)), X])
+    V = np.diagflat(np.product(predProbs, axis=1))
+    covLogit = np.linalg.inv(np.dot(np.dot(X_design.T,V), X_design))
+    return(np.sqrt(np.diag(covLogit)))
+
+def boot(dataFrame, n):
+    standard_errors = []
+    for i in range(n):
+        index = np.random.choice([i for i in range(len(dataFrame))], len(dataFrame))
+        standard_errors.append(boot_se(dataFrame, index))
+    return pd.DataFrame(standard_errors).mean()
+
+print(boot(default,100))
+
