ef3c96b9a512b1be9711549b1b972e92f0c5a047
DumezErika <47596821+DumezErika@users.noreply.github.com>
2020-12-15 10:53:36 +0100

    Update exercice9.py

--- exercices livre/Chapitre 10/exercice9.py
+++ exercices livre/Chapitre 10/exercice9.py
@@ -1,31 +1,7 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn import linear_model
 from random import sample
-import numpy as np
-import statsmodels.api as sm
-import statsmodels.formula.api as smf
-import sklearn.linear_model as lm
-from statsmodels.sandbox.regression.predstd import wls_prediction_std
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import RidgeCV
-from sklearn.linear_model import LassoCV
-from sklearn.decomposition import PCA
-from sklearn.model_selection import cross_val_predict
-from sklearn.metrics import make_scorer
-from sklearn.metrics import SCORERS
-from sklearn.metrics import mean_squared_error
-from sklearn.cross_decomposition import PLSRegression
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.cluster import AgglomerativeClustering
-from sklearn.tree import export_text
 from sklearn import preprocessing
-sns.set_theme(style='darkgrid')
 
 us = pd.read_csv("USArrests.csv")
 X = us
