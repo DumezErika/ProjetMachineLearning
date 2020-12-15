445f7781734f18404cb3a01cc9fc52cf0b89d597
DumezErika <47596821+DumezErika@users.noreply.github.com>
2020-12-15 11:07:03 +0100

    Update exercice9.py

--- exercices livre/Chapitre 8/exercice9.py
+++ exercices livre/Chapitre 8/exercice9.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.tree import export_text
 
