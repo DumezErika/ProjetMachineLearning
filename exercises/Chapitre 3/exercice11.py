9331041a7fba2ac45fecd16ee581cb23c72c5b71
DumezErika <47596821+DumezErika@users.noreply.github.com>
2020-12-16 11:08:07 +0100

    Update exercice11.py

--- exercises/Chapitre 3/exercice11.py
+++ exercises/Chapitre 3/exercice11.py
@@ -11,10 +11,7 @@ y =2*x + np.random.normal(size=100)
 
 df = pd.DataFrame({'x': x, 'y': y})
 
-fig, ax = plt.subplots()
-sns.regplot(x='x', y='y', data=df, scatter_kws={"s": 50, "alpha": 1}, ax=ax)
-ax.axhline(color='gray')
-ax.axvline(color='gray')
+sns.regplot(x='x', y='y')
 
 #a)
 reg = smf.ols('y ~ x + 0', df).fit()
